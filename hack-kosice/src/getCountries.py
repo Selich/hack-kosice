38b1ab7e275735d5f738b0a14533d4de4fad8feb
AIPA10 <bhaanum@gmail.com>
2020-03-28 22:34:08 +0530

    Adding getting country from tweet script

--- hack-kosice/src/getCountries.py
+++ hack-kosice/src/getCountries.py
@@ -1,11 +1,7 @@
-consumer_key = "OYSPnW2fP34yd3Phbvw4yQozk"
-consumer_secret = "15VHmSXkoku7FTz4VBuVfwjtVVf9QxIpRzxjociEUK0jeebROu"
-access_token = "990891304367411201-Ew2E3LGE5dXnuwBxFxDXmZmfIdr1MIO"
-access_token_secret = "7LB5ZPF7LSbxmoC99ERIYiYDLgPT04sQbyh4ggddXpZwP"
-
-import tweepy
+import config
 import datetime
 from geopy.geocoders import Nominatim
+import tweepy
 
 def unique(list1):
 	list_set = set(list1)
@@ -14,31 +10,38 @@ def unique(list1):
 
 def countries(handle):
 
-	userHandle = handle
+	try:
+		userHandle = handle
 
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
-	api = tweepy.API(auth)
+		auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+		auth.set_access_token(config.access_token, config.access_token_secret)
+		api = tweepy.API(auth)
 
-	tweets = api.user_timeline(screen_name=userHandle, count=100, include_rts = False, tweet_mode = 'extended')
+		tweets = api.user_timeline(screen_name=userHandle, count=100, include_rts = False, tweet_mode = 'extended')
 
-	places = []
-	geolocator = Nominatim(user_agent="Twitter Bot") 
+		places = []
+		geolocator = Nominatim(user_agent="Twitter Bot", timeout=3) 
 
-	for tweet in tweets:
-		if ((datetime.datetime.now() - tweet.created_at).days < 15):
-			if (tweet.place != None):
-				coordinates = geolocator.geocode(tweet.place.full_name)
-				location = geolocator.reverse("{}, {}".format(coordinates.latitude, coordinates.longitude), exactly_one=True)
-				address = location.raw['address']
-				places.append(address.get('country', ''))
+		for tweet in tweets:
+			if ((datetime.datetime.now() - tweet.created_at).days < 15):
+				if (tweet.place != None):
+					coordinates = geolocator.geocode(tweet.place.full_name)
+					location = geolocator.reverse("{}, {}".format(coordinates.latitude, coordinates.longitude), exactly_one=True)
+					address = location.raw['address']
+					places.append(address.get('country', ''))
+				else:
+					coordinates = geolocator.geocode(tweet.user.location)
+					location = geolocator.reverse("{}, {}".format(coordinates.latitude, coordinates.longitude), exactly_one=True)
+					address = location.raw['address']
+					places.append(address.get('country', ''))
 			else:
-				coordinates = geolocator.geocode(tweet.user.location)
-				location = geolocator.reverse("{}, {}".format(coordinates.latitude, coordinates.longitude), exactly_one=True)
-				address = location.raw['address']
-				places.append(address.get('country', ''))
-		else:
-			break
+				break
 
-	countries = unique(places)
-	return countries
+		countries = unique(places)
+		return countries
+		
+	except GeocoderTimedOut:
+		return countries(handle)
+		
+handle = "jk_rowling"
+print(countries(handle))
